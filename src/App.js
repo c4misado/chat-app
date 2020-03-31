18bf5e1d6fdd266258f4c47d91a37922c4c7ae89
Alec Adelman <adelmanalec@gmail.com>
2020-03-31 16:34:50 +0300

    made contact list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact image={"https://randomuser.me/api/portraits/men/31.jpg"} firstName="Alexander" lastName="Hill" onlineStatus="offline"/>
-      <Contact image={"https://randomuser.me/api/portraits/men/33.jpg"} firstName="Nicholas" lastName="May" online/>
-      <Contact image={"https://randomuser.me/api/portraits/women/29.jpg"} firstName="Sofia" lastName="Carlson" online/>
+      <Contact avatar={"https://randomuser.me/api/portraits/men/31.jpg"} name="Alexander Hill" onlineStatus="offline"/>
+      <Contact avatar={"https://randomuser.me/api/portraits/men/33.jpg"} name="Nicholas May" online/>
+      <Contact avatar={"https://randomuser.me/api/portraits/women/29.jpg"} name="Sofia Carlson" online/>
+      <ContactList/>
     </div>
   );
 }
-
+/*
 Contact.propTypes = {
   firstName: PropTypes.string,
   lastName: PropTypes.string,
   image: PropTypes.string,
   online: PropTypes.bool
 };
+*/
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,8 @@ class Contact extends React.Component {
     render () {
         return (
             <div className="Contact" >
-                <img className="avatar" src={this.props.image}/>
-                <div className="fn"><p className="name">{this.props.firstName} {this.props.lastName}</p>            
+                <img className="avatar" src={this.props.avatar}/>
+                <div className="fn"><p className="name">{this.props.name}</p>            
                 <div className="status"><div className={this.props.online ? 'status-online' : 'status-offline'}></div>
                 <p className="status-text">{this.props.online ? 'online' : 'offline'}</p></div>
 
