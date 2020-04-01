3d61959fa9d3bca750a8b6179680676a70be5a11
Alec Adelman <adelmanalec@gmail.com>
2020-04-01 15:52:45 +0300

    made online/offline button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,15 +2,30 @@ import React from 'react';
 import './Contact.css';
 
 class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          online: false
+        };
+      }
     render () {
         return (
             <div className="Contact" >
                 <img className="avatar" src={this.props.avatar}/>
                 <div className="fn"><p className="name">{this.props.name}</p>            
-                <div className="status"><div className={this.props.online ? 'status-online' : 'status-offline'}></div>
-                <p className="status-text">{this.props.online ? 'online' : 'offline'}</p></div>
+                <div className="status"><div className={this.state.online ? 'status-online' : 'status-offline'}></div>
+                <button className="status-text"           
+                        onClick={event => {
+                            let oStatus = this.state.online;
+                            oStatus === false ? oStatus = true : oStatus = false;
+                            this.setState({online: oStatus});
+                            }
+                          }
+                >{this.state.online == true ? 'online' : 'offline'}</button>
+                </div>
+            </div>
+            </div>
 
-            </div></div>
         )
     }
 };
